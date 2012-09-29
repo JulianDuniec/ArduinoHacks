5feae6a390b81c5e8bd204b949f541283c1652ce
Julian Duniec <julian.duniec@gmail.com>
2012-09-29 21:16:33 +0200

    ...

--- VoltageDividerAndSensor/VoltageDividerAndSensor.ino
+++ VoltageDividerAndSensor/VoltageDividerAndSensor.ino
@@ -3,6 +3,7 @@ int inputPin = A0;
 
 void setup() 
 {
+  //We send a steady current from output to input
   pinMode(outputPin, OUTPUT);
   pinMode(inputPin, INPUT);
   //Send a steady current though outputPin
@@ -14,6 +15,7 @@ void loop()
 {
   //Read value from input
   int input = analogRead(inputPin);
+  //The input will be affected by the voltage-divider.
   Serial.println("Current read: " + (String)input);
   delay(1000);
 }
